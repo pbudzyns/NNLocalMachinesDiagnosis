130fc2ccfd123e2fcf30faab008ac20acaf97f83
pbudzyns <pawel.budzynski@nokia.com>
2018-10-20 16:13:43 +0200

    prepare SNR to acc table

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ from sklearn.metrics import accuracy_score, f1_score, recall_score, average_prec
 from sklearn.preprocessing import StandardScaler
 import scipy.stats
 import numpy as np
+import os
+import glob
 import matplotlib.pyplot as plt
+import pandas as pd
 
-# import warnings
-# warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+import warnings
+from sklearn.exceptions import DataConversionWarning
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+warnings.filterwarnings(action='ignore', category=DataConversionWarning)
 
 
 def print_scores(y_true, y_pred):
@@ -25,21 +31,22 @@ def print_stats(acc, recall, f1, prec):
 
 
 def get_estimators(signal):
+    label, signal = signal[0], signal[1:]
     maximum = np.max(signal)
     minimum = np.min(signal)
     skewnes = scipy.stats.skew(signal)
     kurtosis = scipy.stats.kurtosis(signal)
     variance = np.var(signal)
     # TODO: add root mean square, crest factor, waveform factor
-    return maximum, minimum, variance, skewnes, kurtosis
+    return label, maximum, minimum, variance, skewnes, kurtosis
 
 
 def extract_from_vector(label, data_vec):
     features = get_estimators(data_vec)
     return label, features
 
-def cross_validate_model(model, data_handler, parts_number):
 
+def cross_validate_model(model, data_handler, parts_number):
     acc, recall, f1, prec = [], [], [], []
     for learning_data, testing_data, learning_labels, testing_labels in data_handler.cross_validation(parts_number):
         learning_data = np.array(list(map(get_estimators, learning_data)))
@@ -53,29 +60,82 @@ def cross_validate_model(model, data_handler, parts_number):
         f1.append(f1_score(testing_labels, predictions))
         prec.append(average_precision_score(testing_labels, predictions))
 
-    return (np.mean(stat) for stat in (acc, recall, f1, prec))
+    return [round(np.mean(stat), 3) for stat in (acc, recall, f1, prec)]
 
+
+def prepare_snr_to_acc_table(model, data_handler, foldername):
+    # files = glob.glob(os.path.join(foldername, "*.csv"))
+    table = pd.DataFrame()
+    pulsation_files = []
+    no_pulsation_file = ''
+
+    for filename in os.listdir(foldername):
+        print(filename)
+        if filename.startswith("nopulsation"):
+            no_pulsation_file = filename
+        else:
+            pulsation_files.append(filename)
+
+    SNRs = []
+    AMPs = []
+    ACCs = []
+    RECALLs = []
+    F1_SCOREs = []
+    data_handler.load_no_pulsation(os.path.join(foldername, no_pulsation_file))
+    for pulsation_file in pulsation_files:
+        print(f'Computing with {pulsation_file}...')
+        data_handler.load_pulsation(os.path.join(foldername, pulsation_file))
+        scores = cross_validate_model(model, data_handler, parts_number=7)
+
+        SNRs.append(pulsation_file.replace(".csv", '').split("_")[-1])
+        AMPs.append(pulsation_file.replace(".csv", '').split("_")[1])
+        ACCs.append(scores[0])
+        RECALLs.append(scores[1])
+        F1_SCOREs.append(scores[2])
+
+    table['SNR'] = SNRs
+    table['IMP_AMP'] = AMPs
+    table['ACC'] = ACCs
+    table['RECALL'] = RECALLs
+    table['F1_SCORE'] = F1_SCOREs
+
+    return table.sort_values(by=['IMP_AMP'])
 
 
 if __name__ == '__main__':
-    handler = SignalsHandler()
-    handler.load_signals('inputs/symulacje_400_50_male')
-    handler.initialize_data_sets()
+    # handler = SignalsHandler(preprocessing=get_estimators)
+    # handler.load_signals('inputs/symulacje_400_50_srednie')
+    # handler.initialize_data_sets()
+    # print(handler.learning_data[0])
+    # print(handler.learning_labels[0])
+    # print(handler.testing_labels[0])
     # TODO: odszumianie
     # TODO: ekstrakcja sygnalu informacyjnego
     # handler.learning_data = np.array(list(map(get_estimators, handler.learning_data)))
     # handler.testing_data = np.array(list(map(get_estimators, handler.testing_data)))
 
-    architectures = [(3,), (4, 3), (7, 5, 3)]
-    for arch in architectures:
-        clf = MLPClassifier(solver='lbfgs',
+    # architectures = [(3,), (4, 3), (7, 5, 3)]
+    # for arch in architectures:
+    #     clf = MLPClassifier(solver='lbfgs',
+    #                     activation='logistic',
+    #                     max_iter=10000,
+    #                     hidden_layer_sizes=arch,
+    #                     learning_rate_init=0.001
+    #                     )
+    #     print(f"TESTING HIDDEN LAYERS: {arch}")
+    #     print_stats(*cross_validate_model(clf, handler, 10))
+
+    handler = SignalsHandler(preprocessing=get_estimators)
+    clf = MLPClassifier(solver='lbfgs',
                         activation='logistic',
                         max_iter=10000,
-                        hidden_layer_sizes=arch,
+                        # hidden_layer_sizes=(7, 6, 3),
+                        hidden_layer_sizes=(6, 3),
                         learning_rate_init=0.001
                         )
-        print(f"TESTING HIDDEN LAYERS: {arch}")
-        print_stats(*cross_validate_model(clf, handler, 10))
+    table = prepare_snr_to_acc_table(clf, handler, "inputs\snr_100_100")
+    print(table)
+    table.to_csv("snr_to_acc.csv")
 
     # plt.plot(handler.pulsation[0])
     # plt.show()
