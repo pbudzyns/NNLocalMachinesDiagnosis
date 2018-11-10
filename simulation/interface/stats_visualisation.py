ed62de46b805ba278f85d0fbd3f7b8e459aa1e5e
pbudzyns <pawel.budzynski@nokia.com>
2018-11-10 14:03:22 +0100

    thesis started

---
+++ simulation/interface/stats_visualisation.py
@@ -0,0 +1,75 @@
+import dash
+from dash.dependencies import Output, Input, State
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_table_experiments as dt
+import plotly.graph_objs as go
+from collections import deque
+import pandas as pd
+import time
+import os
+import agate
+import glob
+from plotly.offline import iplot
+import plotly.io as pio
+import plotly
+
+
+external_stylesheets = ['https://codepen.io/pawelbpw/pen/aRrJRj.css']
+plotly.io.orca.config.executable = r"C:\Users\pbudzyns\AppData\Local\Programs\orca\orca.exe"
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+file_list = [{'label':name.replace("inputs/", ""), 'value':name} for name in glob.glob("inputs/*.csv")]
+
+
+
+
+app.layout = html.Div([
+    dcc.Dropdown(
+        id='my-dropdown',
+        options=file_list,
+        value=file_list[0]['label'],
+        clearable=False,
+    ),
+    dcc.Graph(id='stat-graph', animate=True),
+])
+
+@app.callback(Output('stat-graph', 'figure'),
+              inputs=[Input('my-dropdown', 'value')])
+def update_chart(filename):
+    figure = get_figure(filename)
+    return figure
+
+def get_figure(filename, type='dashboard'):
+    data = []
+    table = pd.read_csv(filename)
+    x = list(table['SNR'])
+    infos = "ACC,RECALL,PRECISION,F1_SCORE".split(',')
+    for info in infos:
+        d = go.Scatter(x=x, y=list(table[info]),
+                       name=info, mode='lines+markers')
+        data.append(d)
+
+    if type == 'dashboard':
+        layout = go.Layout(xaxis=dict(title='SNR'), yaxis=dict(range=(0, 1.1), title='Value'))
+    else:
+        layout = go.Layout(xaxis=dict(title='SNR'), yaxis=dict(range=(0, 1.1), title='Value'), autosize=False,
+                           width=1000, height=500)
+
+    return {'data': data, 'layout': layout}
+
+def plot_all(filenames):
+    for filename in filenames:
+        save_as_pdf(filename)
+
+def save_as_pdf(filename):
+    figure = get_figure(filename, type='pdf')
+    pio.write_image(figure, filename.replace("inputs", "../../latex/images").replace(".csv", ".pdf"))
+
+
+
+
+if __name__ == '__main__':
+    # plot_all(glob.glob("inputs/*.csv"))
+    app.run_server(port=8080, debug=True)
